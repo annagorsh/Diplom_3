2bbe4de78c422a1a475ded47ccb0c6f7a4930494
annagorsh <annagorshhh@gmail.com>
2024-11-06 23:54:42 +0300

    selenium tests

--- links.py
+++ links.py
@@ -9,3 +9,5 @@ FORGOT_PASSWORD_URL = MAIN_URL + "forgot-password"
 RESET_PASSWORD_URL = MAIN_URL + "reset-password"
 
 PROFILE_PAGE_URL = MAIN_URL + "account/profile"
+
+ORDER_FEED_URL = MAIN_URL + "feed"
--- locators.py
+++ locators.py
@@ -18,14 +18,44 @@ class ResetPasswordLocators:
     #Иконка видимости пароля в виде глаза - открытый глаз, пароль закрыт точками
     VIEW_PASSWORD_BUTTON = By.CSS_SELECTOR, ".input__icon > svg: nth - child(1)"
 
-    ###
-    #!завязаться на type = text и type = password в поле ввода
+    #Поле "Пароль" - дефолтное состояние
+    pass
+    #Поле "Пароль" - активное состояние
+    pass
+    #скрытие пароля - можно завязаться на type = text и type = password в поле ввода
+    #активность поля - можно завязаться на input_status_default и input_status_active
 
 #Страница логина
 class LoginPageLocators:
 
     #Кнопка "Восстановить пароль"
     FORGOT_PASSWORD_BUTTON = By.XPATH, "//a[contains(.,'Восстановить пароль')]"
+
+#Главная страница
+class MainPageLocators:
+
+    #Кнопка "Личный кабинет"
+    GO_TO_PROFILE_BUTTON = By.XPATH, "//p[contains(.,'Личный Кабинет')]"
+
+    #Кнопка "Лента заказов"
+    GO_TO_ORDER_FEED_BUTTON = By.XPATH, "//p[contains(.,'Лента Заказов')]"
+
+#Личный кабинет
+class ProfileLocators:
+
+    #Кнопка в хедере для перехода в раздел "История заказов"
+    ORDER_HISTORY_BUTTON = By.XPATH, "//a[contains(.,'История заказов')]"
+
+    #Кнопка "Выход"
+    LOGOUT_BUTTON = By.XPATH, "//button[@type='button'][contains(.,'Выход')]"
+
+#Страница ленты заказов
+class OrderFeedLocators:
+
+    #Кнопка в хедере для перехода в раздел "Конструктор"
+    GO_TO_CONSTRUCTOR_BUTTON = By.XPATH, "//p[contains(.,'Конструктор')]"
+
+
 
 
 
